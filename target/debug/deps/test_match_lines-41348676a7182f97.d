e50c171d4f1145775a2175a983557719eb8361d5
Iryna Rychok <i.rychok@ukma.edu.ua>
2025-11-11 15:06:14 +0200

    added gitignore

---
+++ target/debug/deps/test_match_lines-41348676a7182f97.d
@@ -0,0 +1,9 @@
+/Users/ira/Downloads/practice-iryna-rychok/football match parser/target/debug/deps/test_match_lines-41348676a7182f97.d: tests/test_match_lines.rs Cargo.toml
+
+/Users/ira/Downloads/practice-iryna-rychok/football match parser/target/debug/deps/libtest_match_lines-41348676a7182f97.rmeta: tests/test_match_lines.rs Cargo.toml
+
+tests/test_match_lines.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
